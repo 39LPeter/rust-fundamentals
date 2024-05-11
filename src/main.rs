307530a607750f552f885ec5d9dde72f785a49fd
39LPeter <wlone3949@gmail.com>
2024-05-11 10:55:42 +0300

    RUST

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    //let mut _array=[1,2,3,4,5,6,7,8,9];
+    //let mut index:usize=5;
+    //while index !=0 {
+            //println!("looping through indes{index}items in");
+            //index -=1;
+
+        
+    //}
+    //println!("LIFTOFF!!!");
+    let mut num: i32 =3;
+    while num !=0 {
+        println!("{num}");
+        num -=1;
+        println!("the result is  {num}");
+    }
+    println!("LIFTOFF!!!")
+}
