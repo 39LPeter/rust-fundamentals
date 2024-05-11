ef70cd4c6f4f07b4e0beceee339817e19299412a
39LPeter <wlone3949@gmail.com>
2024-05-11 15:41:28 +0300

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,88 @@
 fn main() {
+    
+        // let name ="Mawia";
+         //let num :i8=10;
+         //let num11 :i8=10;
+         //let num2 :i16=18;
+         //let num11 :u16=19;
+         //let num3 :i32=9;
+         //let num4 :u32=5;
+         //let num5 :i64=45;
+         //let num6 :u64=13;
+         //let num7 :i128=10;
+         //let num8 :u128=15;
+         //let num9 :isize=-10;
+         //let num10 :usize=10;
+        // let number: f32 = 110.56;
+         //let number: f64 = 110.45;
+         //character
+        // let character:char='t';
+         //let character2:char='🎂';
+         //let text_1 : &str = "Welcome to programming";
+         //let text_2: String = String:: from("Hello there..."); // Define TEXT2 as a constant
+         //let text2: String = "Heloooo".to_string(); // Define a new variable text2
+         //println!("hello, {}!", name);
+         //println!("{}",num);
+         //println!("{}",num2);
+         //println!("{}",num4);
+         //println!("{}",num5);
+         //println!("{}",num7);
+         //println!("{}",num8);
+         //println!("{}",num10);
+         //println!("{}",number);
+         //println!("{}",character);
+         //println!("{}",character2);
+          
+         //println!("{}",text_1);yy
+         //println!("Hello...{}",text_2);
+         
+        // let result = sum(5, 10);
+         //println!("Result: {}", result);
+         
+         
+     //}
+     //Functions
+     //#[allow(dead_code)]
+     //fn sum(num_1: i32,num_2: i32){
+         //let  num_3: i32 = num_1+num_2;
+         //println!("the sum of num_one and num_two{}",num_3);
+     //}
+     //fn sum2(num_1: i32,num_2: i32)-> i32{
+        // let  num_3: i32 = num_1+num_2;
+        // println!("the sum of num_one and num_two{}",num_3);
+        // return num_3;
+     //}
+     //fn sum3(num_1: i32,num_2: i32)-> i32{
+        // let  num_3: i32 = num_1+num_2;
+        // println!("the sum of num_one and num_two{}",num_3);
+      //   num_3
+     //}
+     
+     //let b: i32=5;
+     //if a>b {
+         //println!("a is greater than b")
+     //}elif  a==b{
+         //println("a is equal to b")
+     //}
+     //if a b {
+         //println!("a is less than b")
+     //}
+     let mut counter: i32 = 0;
+
+     loop {
+         counter = counter + 1;
+         println!("I am at number {}", counter); // Changed {counter} to {}
+ 
+         if counter == 1 {
+             println!("Starting point");
+         }
+ 
+         if counter == 10 {
+             break;
+         }
+     }
+   
+
     //let mut _array=[1,2,3,4,5,6,7,8,9];
     //let mut index:usize=5;
     //while index !=0 {
@@ -8,11 +92,11 @@ fn main() {
         
     //}
     //println!("LIFTOFF!!!");
-    let mut num: i32 =3;
-    while num !=0 {
-        println!("{num}");
-        num -=1;
-        println!("the result is  {num}");
-    }
-    println!("LIFTOFF!!!")
+    //let mut num: i32 =3;
+    //while num !=0 {
+        //println!("{num}");
+        //num -=1;
+        //println!("the result is  {num}");
+//}
+    //println!("LIFTOFF!!!")
 }
